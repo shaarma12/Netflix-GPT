364a0140de46bbe26061cc3918cbfeaa13c18180
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-10-07 23:24:15 +0530

    Netflix-GPT Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-red-500 m-5">
+        Welcome! Your Netflix is here.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
